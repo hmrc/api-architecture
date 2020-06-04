1008bab2302aeb37dd2a06d291108f4695f4ec92
Toby Porter <toby.porter@capgemini.com>
2020-06-04 09:15:57 +0100

    Fix html generation

--- generate-html.sh
+++ generate-html.sh
@@ -3,9 +3,9 @@
 MODULE=$1
 
 sed -i "/version:/c\version: $(date '+%Y-%m-%d %H:%M')" ${MODULE}/${MODULE}.raml
-npx raml2html --theme raml2html-printable-theme --input ${MODULE}/${MODULE}.raml --output html_docs_printable/${MODULE}-printable.html
+npx raml2html --theme raml2html-printable-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-printable.html
 cp html_docs_printable/${MODULE}-printable.html ../html_docs_printable
-npx raml2html --theme raml2html-default-theme --input ${MODULE}/${MODULE}.raml --output html_docs_compact/${MODULE}-compact.html
+npx raml2html --theme raml2html-default-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-compact.html
 cp html_docs_compact/${MODULE}-compact.html ../html_docs_compact
 
 rm html_docs.zip
