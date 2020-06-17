f4bc3615af36d497ec78dc0ef89bb68d121b2b19
Toby Porter <toby.porter@capgemini.com>
2020-06-17 14:51:51 +0100

    Expenses updated

--- generate-html.sh
+++ generate-html.sh
@@ -5,11 +5,11 @@ MODULE=$1
 #https://www.npmjs.com/package/raml2html-slate-theme
 
 sed -i "/version:/c\version: $(date '+%Y-%m-%d %H:%M')" ${MODULE}/${MODULE}.raml
-#npx raml2html --theme raml2html-printable-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-printable.html
+raml2html --theme raml2html-printable-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-printable.html
 #cp html_docs_printable/${MODULE}-printable.html ../html_docs_printable
 
 #npx
-#raml2html --theme raml2html-default-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-compact.html
+raml2html --theme raml2html-default-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-compact.html
 #cp html_docs_compact/${MODULE}-compact.html ../html_docs_compact
 
 #raml2html --theme raml2html-werk-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-werk.html
@@ -17,8 +17,8 @@ sed -i "/version:/c\version: $(date '+%Y-%m-%d %H:%M')" ${MODULE}/${MODULE}.raml
 raml2html --theme raml2html-slate-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-slate.html
 #raml2html --theme raml2html-slate-modified-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-slate-modified.html
 raml2html --theme raml2html-modern-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-modern.html
-raml2html --theme raml2html-kaa-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-kaa.html
-raml2html --theme raml2html-confluencewiki-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-confluencewiki.html
+#raml2html --theme raml2html-kaa-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-kaa.html
+#raml2html --theme raml2html-confluencewiki-theme --logo MTD-logo.png --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-confluencewiki.html
 #npx
 #raml2html --theme raml2html-material-theme --input ${MODULE}/${MODULE}.raml --output  ${MODULE}/${MODULE}-material.html
 
